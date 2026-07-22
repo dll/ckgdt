cb8f59fd24da5a4b5139d8b486a92fc5012b3312
ldl <ldl@chzu.edu.cn>
2026-07-22 21:08:50 +0800

    chore: 升版 v3.1.4

--- lib/core/version.dart
+++ lib/core/version.dart
@@ -1,10 +1,10 @@
 class Version {
   static const int major = 3;
   static const int minor = 1;
-  static const int patch = 3;
+  static const int patch = 4;
   static const int build = 0;
 
-  static const String display = '3.1.3';
-  static const String pubspec = '3.1.3+0';
-  static const String platform = '3.1.3';
+  static const String display = '3.1.4';
+  static const String pubspec = '3.1.4+0';
+  static const String platform = '3.1.4';
 }
